f7db7b6650ffa78be45c4104d9c078ae762487ec
tonsojun <josephjun@gmail.com>
2021-03-20 11:26:17 -0700

    Completed intro_10

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,11 @@ class PagesController < ApplicationController
 
 
   def show
-    new_params = {}
-    new_params[:name] = params['']
-    new_params[:name][:hobby] = params['']
-    new_params[:name][:favorite_food] = params['']
+    new_params = { name: '', hobby: ''}
+    new_params[:name] = params[:name]
+    new_params[:hobby] = params[:hobby]
+    new_params[:favorite_food] = params[:favorite_food]
+    new_params[:favorite_color] = params[:favorite_color]
     session[:students] = new_params
     redirect_to "/"
   end

6567949c00d96d7a3b80df18d81740ff43ed59a4
tonsojun <josephjun@gmail.com>
2021-03-21 12:49:19 -0700

    Fixed view, able to see data in table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,25 +1,18 @@
 class PagesController < ApplicationController
   def main
+    @student = Student.all
   end
-=begin
-  def show
-    new_params = { name: '', hobby: '', favorite_food: '', favorite_color: ''}
-    new_params[:name] = params[:name]
-    new_params[:hobby] = params[:hobby]
-    new_params[:favorite_food] = params[:favorite_food]
-    new_params[:favorite_color] = params[:favorite_color]
-    session[:students] = new_params
-    redirect_to "/"
-  end
-=end
+
   def index
-    @students = Student.all
+
   end
 
   def new
     @student = Student.new(name: params[:name], hobby: params[:hobby])
     @student.save
   end
+  
+  
 
 
 end
